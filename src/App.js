4aa4cce5e5e4b3e08b8157c269d132baccc97fa2
wodjafink <wodjafink@gmail.com>
2018-07-10 18:18:08 -0700

    Okay, the score and top score stuff is working as I want it to, though getting the arrays tracked in the state was a bit frustrating

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from "./components/Card"
+import Wrapper from "./components/Wrapper";
 import characters from "./characters.json"
 
 class App extends Component {
 
   state = {
     score: 0,
-    friendsArray: []
+    topScore: 0,
+    drawCardsArray: [0],
+    clickedCardsArray: [0]
   };
 
   componentDidMount() {
-    
+
   }
 
+  updateTopScore = () =>{
+
+    if (this.state.score > this.state.topScore){
+      // console.log("Top is " + this.state.topScore);
+      this.setState({topScore: this.state.score + 0});
+    }
+  }
+
+  clickCard = id => {
+
+    var isInArray = false;
+
+    let clickedCardsArray = this.state.clickedCardsArray;
+
+    for (var i = 0; i < clickedCardsArray.length; i++){
+      if (clickedCardsArray[i] === id)
+        isInArray = true;
+    }
+
+    if (isInArray){
+      // Score gets set back to zero
+      this.setState({score: 0});
+      console.log("You've already clicked this one");
+    } else {
+      console.log("Haven't clicked this one yet");
+      // Add to the clicked array
+      this.setState({
+        clickedCardsArray: [...this.state.clickedCardsArray, id]
+      })
+
+      // Add to score
+      this.setState({score: this.state.score + 1}, this.updateTopScore);
+      // console.log("Array: ")
+      // for (var j = 0; j < clickedCardsArray.length; j++){
+      //   console.log(clickedCardsArray[j])
+      // }
+    }
+
+
+
+  }
 
   render() {
     return (
@@ -28,7 +72,7 @@ class App extends Component {
                 <h2>Click an image to begin!</h2>
               </div>
               <div className="col-md-4">
-                <h2>Score: 0 | Top Score: 0</h2>
+                <h2>Score: {this.state.score} | Top Score: {this.state.topScore}</h2>
               </div>
             </div>
           </div>
@@ -37,16 +81,17 @@ class App extends Component {
           <h1>Clicky Game!</h1>
           <p>Click on an image to earn points, but don't click on any more than once!</p>
         </div>
-        <div>
-          {characters.map(character => (
-            <Card 
-              id={character.id}
-              key={character.id}
-              name={character.name}
-              image={character.image}
-            />
-          ))}
-        </div>
+        <Wrapper>
+            {characters.map(character => (
+              <Card 
+                clickCard={this.clickCard}
+                id={character.id}
+                key={character.id}
+                name={character.name}
+                image={character.image}
+              />
+            ))}
+        </Wrapper>
       </div>
     );
   }
